601bb1b2f107f1f694b0c5af96967765d0b2f23c
Joshua Da Costa <Joshua.0331@hotmail.com>
2022-07-19 19:11:27 -0400

    add side effect bump animation

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,14 +1,36 @@
 import classes from "./HeaderCartButton.module.css";
 import CartIcon from "../UI/CartIcon";
 import CartContext from "../../store/cart-context";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 const HeaderCartButton = (props) => {
   const cartCtx = useContext(CartContext);
-  const cartAmount = cartCtx.items.reduce((currentNum, item) => {
+  const { items } = cartCtx;
+
+  const cartAmount = items.reduce((currentNum, item) => {
     return currentNum + item.amount;
-  }, 0)
+  }, 0);
+
+  const [amountChanged, setAmountChanged] = useState(false);
+
+  const btnClasses = `${classes.button} ${amountChanged ? classes.bump : ""}`;
+
+
+  useEffect(() => {
+    if (items.length === 0) return;
+    setAmountChanged(true);
+
+    const id = setTimeout(() => {
+    setAmountChanged(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(id);
+    }
+
+  }, [items]);
+
   return (
-    <button onClick={() => props.toggleCart()} className={classes.button}>
+    <button onClick={() => props.toggleCart()} className={btnClasses}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -8,25 +8,24 @@ const initialState = {
 
 const cartReducer = (state, action) => {
   const { payload } = action;
-  console.log(state);
+  console.log(payload);
   if (action.type === "ADD_ITEM") {
     const updatedTotalAmount =
       state.totalAmount + payload.price * payload.amount;
 
-    const existingItemsIndex = state.items.findIndex(item => {
+    const existingItemsIndex = state.items.findIndex((item) => {
       return item.id === payload.id;
     });
 
     const existingItem = state.items[existingItemsIndex];
     let updatedItems;
-    if(existingItem) {
+    if (existingItem) {
       const updatedItem = {
         ...existingItem,
-        amount: existingItem.amount + payload.amount
+        amount: existingItem.amount + payload.amount,
       };
       updatedItems = [...state.items];
       updatedItems[existingItemsIndex] = updatedItem;
-
     } else {
       updatedItems = state.items.concat(payload);
     }
@@ -34,8 +33,33 @@ const cartReducer = (state, action) => {
       items: updatedItems,
       totalAmount: updatedTotalAmount,
     };
-  } else if (action.type === "REMOVE_ITEM") {
-  
+  }
+
+  if (action.type === "REMOVE_ITEM") {
+    const existingItemIndex = state.items.findIndex((item) => {
+      return item.id === payload;
+    });
+
+    const existingItem = state.items[existingItemIndex];
+    const updatedTotalAmount = state.totalAmount - existingItem.price;
+    let updatedItems;
+    if(existingItem.amount === 1) {
+      updatedItems = state.items.filter(item => {
+        return item.id !== payload;
+      });
+    } else {
+      const updatedItem = {
+        ...existingItem,
+        amount: existingItem.amount - 1
+      };
+      updatedItems = [...state.items];
+      updatedItems[existingItemIndex] = updatedItem;
+    }
+
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
+    };
   }
 
   return initialState;
